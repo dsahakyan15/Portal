8cbb8ee32e401fa1d2bd1622f07a5fbc7dfc3f01
David Sahakyan <davitsahakyan373@gmail.com>
2023-10-16 21:37:11 +0400

    dd

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import React from "react"
+import ReactDOM from 'react-dom/client'
 import Some from "./some";
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<h1>Barev dzez</h1>)
 
 Some(3)
